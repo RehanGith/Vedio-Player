73307dddf757dba76a7f56aff4e01eb0efeec958
RehanGith <chrehan1007@gmail.com>
2024-12-28 23:33:41 +0700

    Created VedioAdapter to hold Recyle View of videos

--- app/src/main/java/com/example/vxplayer/VideoAdapter.kt
+++ app/src/main/java/com/example/vxplayer/VideoAdapter.kt
@@ -1,6 +1,27 @@
 package com.example.vxplayer
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vxplayer.databinding.VideoViewBinding
 
-class VideoAdapter(private val context: Context, private val videoList: ArrayList<String>){
+class VideoAdapter(private val context: Context, private val videoList: ArrayList<String>):
+    RecyclerView.Adapter<VideoAdapter.MyHolder>() {
+    class MyHolder(binding: VideoViewBinding): RecyclerView.ViewHolder(binding.root) {
+        val title = binding.tvVideoName
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        return MyHolder(VideoViewBinding.inflate(LayoutInflater.from(context), parent, false)   )
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        holder.title.text = videoList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return videoList.size
+    }
+
 }
--- app/src/main/java/com/example/vxplayer/VideosFragment.kt
+++ app/src/main/java/com/example/vxplayer/VideosFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.vxplayer.databinding.FragmentVideosBinding
 
 
 class VideosFragment : Fragment() {
@@ -13,7 +15,18 @@ class VideosFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videos, container, false)
+        val view = inflater.inflate(R.layout.fragment_videos, container, false)
+        val binding = FragmentVideosBinding.bind(view)
+        val tempList = ArrayList<String>()
+        tempList.add("First video")
+        tempList.add("second video")
+        tempList.add("third video")
+        tempList.add("forth video")
+        tempList.add("fifth video")
+        binding.rvView.setItemViewCacheSize(10)
+        binding.rvView.setHasFixedSize(true)
+        binding.rvView.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvView.adapter = VideoAdapter(requireContext(), tempList)
+        return view
     }
 }
