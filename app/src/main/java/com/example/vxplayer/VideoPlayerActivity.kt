29682bdcc7834035e1ced1aab528545bc2e331c3
RehanGith <chrehan1007@gmail.com>
2024-12-30 21:13:16 +0700

    made change and added new libraries

--- app/src/main/java/com/example/vxplayer/VideoPlayerActivity.kt
+++ app/src/main/java/com/example/vxplayer/VideoPlayerActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vxplayer.databinding.ActivityVideoPlayerBinding
 
 class VideoPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_video_player)
+        val binding = ActivityVideoPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
